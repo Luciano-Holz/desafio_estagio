8b3a8878f34eee49dd43198ae975ebd87ee19b68
Luciano Holz <luciano.holz@hotmail.com>
2021-10-03 14:44:59 -0300

    Add method add on class City

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const customExpress = require('./config/customExpress');
 //const conection 
 const connection = require('./infra/connection');
 
+const Tablees = require('./infra/tablees')
+
 //connecting with file conection
 connection.connect(erro => {
     if(erro) {
         console.log("Not connected to the database.", erro);
     } else {
         console.log('Successfully connected!');
+        //connection in use
+        Tablees.init(connection);
         //instance customExpress with const app
         const app = customExpress();
         //server listen at port 3000
