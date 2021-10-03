5a7dab6c099db062a4302a9002c472e4d2e90ad6
Luciano Holz <luciano.holz@hotmail.com>
2021-10-03 20:16:59 -0300

    Adding get(name cities) route from city table

--- models/indexCity.js
+++ models/indexCity.js
@@ -3,6 +3,8 @@
 const connection = require('../infra/connection');
 //class to insert data into the city table
 class City {
+    //Routes for table city
+    //city ​​class add method
     add(city, res) {
         const sql = 'INSERT INTO city SET ?';
 
@@ -14,6 +16,7 @@ class City {
             }
         } )
     }
+    //city ​​class list method
     list(res) {
         const sql = `SELECT * FROM city`;
 
@@ -25,20 +28,21 @@ class City {
             }
         } )
     }
-    // stateList(res) {
+    //city ​​class nameList method
+    nameList(res) {
+        const sql = 'SELECT name FROM city';
+
+        connection.query(sql, (error, results) => {
+            if(error) {
+                res.status(400).json(error);
+            } else {
+                res.status(200).json(results);
+            }
+        } )
+    }
+
+      // stateList(res) {
     //     const sql = `SELECT state FROM city`;
-
-    //     connection.query(sql, (error, results) => {
-    //         if(error) {
-    //             res.status(400).json(error);
-    //         } else {
-    //             res.status(200).json(results);
-    //         }
-    //     } )
-    // }
-
-    // nameList(res) {
-    //     const sql = 'SELECT name FROM city';
 
     //     connection.query(sql, (error, results) => {
     //         if(error) {
