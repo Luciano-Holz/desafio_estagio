93d530963613b3c9715a53fc631151e0c5d2a322
Luciano Holz <luciano.holz@hotmail.com>
2021-10-03 20:47:03 -0300

    Changed table fields

--- infra/tablees.js
+++ infra/tablees.js
@@ -1,34 +1,35 @@
-
+//File to creation tables
+//class Tables with the init method that will start and receive the database connection
 class Tablees {
     init(connection) {
         this.connection = connection;
         this.createCity();
         this.createClient();
     }
-
+    //creating table city
     createCity() {
         const sql = 'CREATE TABLE IF NOT EXISTS city (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(50) NOT NULL, state VARCHAR(30) NOT NULL, PRIMARY KEY(id))';
 
-        this.connection.query(sql, erro => {
-            if(erro) {
-                console.log('Table city not created.', erro);
+        this.connection.query(sql, error => {
+            if(error) {
+                console.log('Table city not created.', error);
             } else {
                 console.log('Table city created successfully!');
             }
         })
     }
-
+    //creating table client
     createClient() {
-    const sql = 'CREATE TABLE IF NOT EXISTS client (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, gender ENUM("Masculino", "Feminino", "Outro", "Prefiro não dizer"), name VARCHAR(100) NOT NULL, birth_date DATE NOT NULL, age INT NOT NULL, city INT, FOREIGN KEY(city) REFERENCES city(id) ON DELETE CASCADE)';
+    const sql = 'CREATE TABLE IF NOT EXISTS client (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100) NOT NULL, gender ENUM("Masculino", "Feminino", "Outro", "Prefiro não dizer"), birth_date DATE NOT NULL, age INT NOT NULL, city INT, FOREIGN KEY(city) REFERENCES city(id) ON DELETE CASCADE)';
     
-    this.connection.query(sql, erro => {
-        if(erro) {
-            console.log('Table client not created.', erro);
+    this.connection.query(sql, error => {
+        if(error) {
+            console.log('Table client not created.', error);
         } else {
             console.log('Table client created successfully!');
         }
     });
     }
 }
-
+//exports class Tablees
 module.exports = new Tablees;
