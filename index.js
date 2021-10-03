e8970a6f89b02c45f567ff2727cc4a00b3b55e5d
Luciano Holz <luciano.holz@hotmail.com>
2021-10-03 09:50:36 -0300

    Add const connection and connecting with file conection

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+//Main application file
 //import module customExpress
 const customExpress = require('./config/customExpress');
-//instance customExpress with const app
-const app = customExpress();
+//const conection 
+const connection = require('./infra/connection');
 
+//connecting with file conection
+connection.connect(erro => {
+    if(erro) {
+        console.log("Not connected to the database.", erro);
+    } else {
+        console.log('Successfully connected!');
+        //instance customExpress with const app
+        const app = customExpress();
+        //server listen at port 3000
+        app.listen(3000, () => console.log("Server listening on port 3000"));
+    }
+})
 
-//server listen at port 3000
-app.listen(3000, () => console.log("Server listen at port 3000"));
