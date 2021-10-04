d374457f7d79fa3914e44039555a223182401a1e
Luciano Holz <luciano.holz@hotmail.com>
2021-10-04 15:35:55 -0300

    Adding searchByName method from table client

--- models/indexClient.js
+++ models/indexClient.js
@@ -1,20 +1,49 @@
 //File responsible for connecting with necessary methods
 //import module connection
 const connection = require('../infra/connection');
+//import module moment
+const moment = require('moment');
 //class to insert data into the client table
 class Client {
     //Methods for table client
-    //client ​​class add method
+    //client ​​class add method with validations
     add(client, res) {
-        const sql = 'INSERT INTO client SET ?';
+        const birth_date = moment(client.birth_date, 'DD-MM-YYYY').format('YYYY-MM-DD');
+        const birthdate = moment(birth_date);
 
-        connection.query(sql, client, (error, results) => {
-            if(error) {
-                res.status(400).json(error);
-            } else {
-                res.status(201).json(client);
-            }
-        })
+        const name = client.name;
+        const nameValid = name.length >=3;
+
+        const validations = [
+            {
+                name: "birth_date",
+                valid: birthdate,
+                message: "The date is incorrect."
+             },
+            {
+                name: "name",
+                valid: nameValid,
+                message: "The customer name must be at least 3 characters long."
+            }  
+        ]
+        const errors = validations.filter(field => !field.valid);
+        const existsErrors = errors.length;
+
+        if(existsErrors) {
+            res.status(400).json(errors);
+        } else {
+            const clientAndDateCorrect = {...client, birth_date, name}
+
+            const sql = 'INSERT INTO client SET ?';
+
+            connection.query(sql, clientAndDateCorrect, (error, results) => {
+                if(error) {
+                    res.status(400).json(error);
+                } else {
+                    res.status(201).json(client);
+                }
+            })
+        }   
     }
     //client class lists method
     lists(res) {
@@ -28,8 +57,21 @@ class Client {
             }
         })
     }
-    //client class search method
-    search(id, res) {
+    //client class searchByName method
+    searchByName(name, res) {
+        const sql = `SELECT * FROM client WHERE name=${name}`;
+
+        connection.query(sql, (error, results) => {
+            const client = results[0];
+            if(error) {
+                res.status(400).json(error);
+            } else {
+                res.status(200).json(client);
+            }
+        })
+    }
+    //client class searchById method
+    searchById(id, res) {
         const sql = `SELECT * FROM client WHERE id=${id}`;
 
         connection.query(sql, (error, results) => {
